4f465e2f0894d40246cf69c9ef3bfa8770409f79
Christopher <timm@effective-webwork.de>
2025-02-03 14:35:01 +0100

    Update ext_localconf.php

--- ext_localconf.php
+++ ext_localconf.php
@@ -6,7 +6,7 @@ if (!defined('TYPO3_MODE')) {
 \FluidTYPO3\Flux\Core::registerProviderExtensionKey('Kitodo.PresentationPackage', 'Content');
 \FluidTYPO3\Flux\Core::registerProviderExtensionKey('Kitodo.PresentationPackage', 'Page');
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('presentation_package','Configuration/TypoScript', 'Presentation Package');
+//\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('presentation_package','Configuration/TypoScript', 'Presentation Package');
 
 // Override language files
 $GLOBALS['TYPO3_CONF_VARS']['SYS']['locallangXMLOverride']['EXT:dlf/Resources/Private/Language/locallang_metadata.xlf'][] = 'EXT:presentation_package/Resources/Private/Language/Overrides/locallang_metadata.xlf';
